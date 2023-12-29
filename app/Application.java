2bca61fa66b883f5483a7d7981eff6a1a5bf81e9
Jackson Rodrigues <jacksonrodrigues@fitec.org.br>
2023-12-28 22:12:40 -0300

    feat: matcher and pattern

--- app/Application.java
+++ app/Application.java
@@ -1,9 +1,19 @@
 package app;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
 
     public static void main (String[] args) {
-        System.out.print("Olá mundo!");
+        String regex = "Olá mundo!";
+        String m = "do";
+        Pattern pattern = Pattern.compile(m, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(regex);
+
+        while (matcher.find()) {
+            System.out.println(matcher.start());
+        }
     }
 
 }
